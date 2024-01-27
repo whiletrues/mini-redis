d07a217cfa81743a64211ab14cf5f5dd397d9e50
whiletrue <baptiste.isenbrandt@gmail.com>
2024-01-27 14:25:43 +0100

    intial commit

---
+++ miniredis/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"miniredis/src/server"
+)
+
+func main() {
+	server.PrintHello()
+	fmt.Println("Hello, Modules!")
+}
---
+++ miniredis/src/server/server.go
@@ -0,0 +1,7 @@
+package server
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello, Modules! This is mypackage speaking!")
+}
