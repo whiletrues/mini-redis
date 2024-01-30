52ff0ca52f7c0f77ca5cfaa9f7cb533a502ee2de
whiletrue <baptiste.isenbrandt@gmail.com>
2024-01-30 17:54:44 +0100

    Add basic server ePoll for tcp handle

--- miniredis/main.go
+++ miniredis/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"miniredis/src/common"
+	"miniredis/src/server"
 )
 
 func main() {
 	fmt.Println("Hello, Modules!")
+
+	err := server.StartServer("127.0.0.1", 5093)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	Parser := common.NewParser()
 
 	Parser.Parse([]byte("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"))
-
-	
 }
--- miniredis/src/server/server.go
+++ miniredis/src/server/server.go
@@ -2,48 +2,140 @@ package server
 
 import (
 	"fmt"
-	"miniredis/src/common"
 	"net"
+	"strconv"
+	"syscall"
 )
 
 type server struct {
 	listener net.Listener
 }
 
-func StartServer() {
-	ln, err := net.Listen("tcp", ":6380")
+type Socket struct {
+	FileDescriptor int
+}
+
+func (socket Socket) Read(buffer []byte) (int, error) {
+	if len(buffer) == 0 {
+		return 0, nil
+	}
+
+	readCount, err :=
+		syscall.Read(socket.FileDescriptor, buffer)
+	if err != nil {
+		readCount = 0
+	}
+	return readCount, err
+}
+
+func (socket Socket) Write(buffer []byte) (int, error) {
+	Writecount, err := syscall.Write(socket.FileDescriptor, buffer)
+	if err != nil {
+		Writecount = 0
+	}
+	return Writecount, err
+}
+
+func (socket Socket) Close() error {
+	return syscall.Close(socket.FileDescriptor)
+}
+
+func (socket *Socket) String() string {
+	return strconv.Itoa(socket.FileDescriptor)
+}
+
+func listen(ip string, port int) (*Socket, error) {
+
+	socket := &Socket{}
+
+	socketFileDescriptor, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+
+	socket.FileDescriptor = socketFileDescriptor
+	if err != nil {
+		return nil, fmt.Errorf("failed to create socket (%v)", err)
+	}
+
+	socketAddress := &syscall.SockaddrInet4{Port: port}
+	copy(socketAddress.Addr[:], net.ParseIP(ip).To4())
+
+	if err = syscall.Bind(socket.FileDescriptor, socketAddress); err != nil {
+		return nil, fmt.Errorf("failed to bind socket (%v)", err)
+	}
+
+	if err = syscall.Listen(socket.FileDescriptor, syscall.SOMAXCONN); err != nil {
+		return nil, fmt.Errorf("failed to listen on socket (%v)", err)
+	}
+
+	return socket, nil
+}
+
+func StartServer(ip string, port int) (err error) {
+	socket, err := listen(ip, port)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return fmt.Errorf("failed to start server (%v)", err)
+	}
+
+	epollDescriptor, err := syscall.EpollCreate(socket.FileDescriptor)
+
+	if err != nil {
+		return fmt.Errorf("epoll creation failed (%v)", err)
+	}
+
+	var event syscall.EpollEvent
+
+	event.Events = syscall.EPOLLIN
+	event.Fd = int32(socket.FileDescriptor)
+
+	defer syscall.Close(epollDescriptor)
+
+	if err = syscall.EpollCtl(epollDescriptor, syscall.EPOLL_CTL_ADD, socket.FileDescriptor, &event); err != nil {
+		return fmt.Errorf("epoll control failed (%v)", err)
 	}
 
 	for {
+		var events [1]syscall.EpollEvent
 
-		con, err := ln.Accept()
+		_, err = syscall.EpollWait(epollDescriptor, events[:], -1)
 
 		if err != nil {
-			fmt.Println(err)
-			continue
+			return fmt.Errorf("epoll wait failed (%v)", err)
 		}
 
-		go handleConnection(con)
+		for _, event := range events {
+			if event.Fd == int32(socket.FileDescriptor) {
+				_, _, err := syscall.Accept(socket.FileDescriptor)
+
+				if err != nil {
+					return fmt.Errorf("accept failed (%v)", err)
+				}
+
+				go handleConnection(socket)
+			}
+		}
 	}
 }
 
-func handleConnection(con net.Conn) {
-	defer con.Close()
+func handleConnection(connection *Socket) error {
+	defer connection.Close()
 
-	buffer := make([]byte, 1024)
+	var buffer [1024]byte
 
-	_, err := con.Read(buffer)
+	for {
+		readCount, err := connection.Read(buffer[:])
 
-	if err != nil {
-		fmt.Println(err)
-		return
+		if err != nil {
+			return fmt.Errorf("read failed (%v)", err)
+		}
+
+		if readCount == 0 {
+			return nil
+		}
+
+		_, err = connection.Write(buffer[:readCount])
+
+		if err != nil {
+			return fmt.Errorf("write failed (%v)", err)
+		}
 	}
-
-	parser := common.NewParser()
-
-	parser.Parse(buffer)
 }
